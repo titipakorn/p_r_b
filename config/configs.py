b2cdcbbb50ff0720bc7346b3281a098b97b70c75
Prince <titipakorn.p@gmail.com>
2020-04-02 16:30:22 +0700

    update config

--- config/configs.py
+++ config/configs.py
@@ -36,8 +36,8 @@ class Config(DefaultConfig):
         self.RERANKING = True
 
         self.sct_config = dict(
-            time_window=30,
-            continue_time_thresh=60,
+            time_window=10,
+            continue_time_thresh=2,
             track_clear_thresh=300,
             match_threshold=0.25,
             merge_thresh=0.15,
